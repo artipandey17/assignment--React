6fffce20beaa1425363763be46be01e9d88a9274
Arti Pandey <artipandey1712@gmail.com>
2022-02-01 15:46:16 +0530

    code 7

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Home from "./component/Home"
 import { BrowserRouter as Router,Route,Redirect} from "react-router-dom"
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle"
+
 import Navbar from "./component/Navbar"
 import Menu from "./component/Menu"
 
